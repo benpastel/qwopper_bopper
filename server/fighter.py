bb27035f0788ac62671b7e61ff345aa8ecd0b666
benji <benpastel@gmail.com>
2024-02-17 18:15:44 -0800

    split fighter out; loop over players and websockets in game loop

---
+++ server/fighter.py
@@ -0,0 +1,108 @@
+from typing import NamedTuple
+
+import pymunk  # type: ignore
+
+TORSO_SIZE = (164, 254)
+LEG_SIZE = (60, 275)
+
+# how many pixels from each edge
+# are the torso & leg anchors?
+LEG_JOINT_OFFSET = 30
+
+
+class Fighter(NamedTuple):
+    torso: pymunk.Body
+    torso_box: pymunk.Poly
+
+    # right leg
+    rleg: pymunk.Body
+    rleg_box: pymunk.Poly
+    rleg_motor: pymunk.SimpleMotor
+
+    # left leg
+    lleg: pymunk.Body
+    lleg_box: pymunk.Poly
+    lleg_motor: pymunk.SimpleMotor
+
+    def position_json(self) -> dict[str, dict[str, float]]:
+        """
+        Encode position in json for sending to client.
+        """
+        torso_x, torso_y = self.torso.position
+        rleg_x, rleg_y = self.rleg.position
+        lleg_x, lleg_y = self.lleg.position
+
+        return {
+            "torso": {"x": torso_x, "y": torso_y, "angle": self.torso.angle},
+            "rleg": {"x": rleg_x, "y": rleg_y, "angle": self.rleg.angle},
+            "lleg": {"x": lleg_x, "y": lleg_y, "angle": self.lleg.angle},
+        }
+
+
+def add_fighter(space: pymunk.Space, group: int) -> Fighter:
+    torso = pymunk.Body(mass=10, moment=1000)
+    torso.position = 100, 100
+    torso.angle = 0
+
+    rleg = pymunk.Body(mass=10, moment=500)
+    rleg.position = 100, 100
+    rleg.angle = 0
+
+    lleg = pymunk.Body(mass=10, moment=500)
+    lleg.position = 100, 100
+    lleg.angle = 0
+
+    torso_box = pymunk.Poly.create_box(torso, size=TORSO_SIZE)
+    torso_box.group = group
+    torso_box.elasticity = 0.5
+
+    rleg_box = pymunk.Poly.create_box(rleg, size=LEG_SIZE)
+    rleg_box.group = group
+    rleg_box.elasticity = 0.5
+
+    lleg_box = pymunk.Poly.create_box(lleg, size=LEG_SIZE)
+    lleg_box.group = group
+    lleg_box.elasticity = 0.5
+
+    torso_box.filter = pymunk.ShapeFilter(group=group)
+    rleg_box.filter = pymunk.ShapeFilter(group=group)
+    lleg_box.filter = pymunk.ShapeFilter(group=group)
+
+    rtorso_anchor = (
+        TORSO_SIZE[0] / 2 - LEG_JOINT_OFFSET,
+        TORSO_SIZE[1] / 2 - LEG_JOINT_OFFSET,
+    )
+    ltorso_anchor = (
+        LEG_JOINT_OFFSET - TORSO_SIZE[0] / 2,
+        TORSO_SIZE[1] / 2 - LEG_JOINT_OFFSET,
+    )
+    rleg_anchor = LEG_JOINT_OFFSET - LEG_SIZE[0] / 2, LEG_JOINT_OFFSET - LEG_SIZE[1] / 2
+    lleg_anchor = LEG_SIZE[0] / 2 - LEG_JOINT_OFFSET, LEG_JOINT_OFFSET - LEG_SIZE[1] / 2
+    rjoint = pymunk.PivotJoint(torso, rleg, rtorso_anchor, rleg_anchor)
+    ljoint = pymunk.PivotJoint(torso, lleg, ltorso_anchor, lleg_anchor)
+
+    rleg_motor = pymunk.SimpleMotor(torso, rleg, rate=0)
+    lleg_motor = pymunk.SimpleMotor(torso, lleg, rate=0)
+
+    space.add(
+        torso,
+        torso_box,
+        rleg,
+        rleg_box,
+        rleg_motor,
+        lleg,
+        lleg_box,
+        lleg_motor,
+        rjoint,
+        ljoint,
+    )
+    return Fighter(
+        torso,
+        torso_box,
+        rleg,
+        rleg_box,
+        rleg_motor,
+        lleg,
+        lleg_box,
+        lleg_motor,
+    )
---
+++ server/game.py
@@ -0,0 +1,166 @@
+from time import time
+import json
+from enum import Enum
+from websockets.server import WebSocketServerProtocol
+
+import asyncio
+
+import pymunk  # type: ignore
+
+from server.fighter import Fighter, add_fighter
+
+
+class Player(str, Enum):
+    RED = "red"
+    BLUE = "blue"
+
+
+def other_player(p: Player) -> Player:
+    return Player.RED if p == Player.BLUE else Player.BLUE
+
+
+WIDTH = 1600
+HEIGHT = 800
+FPS = 60
+IMPULSE = 1000
+GRAVITY = 500
+STEPS_PER_FRAME = 10
+
+WALL_GROUP = 1
+RED_GROUP = 2
+BLUE_GROUP = 3
+
+# For now we support at most one game at a time.
+# this tracks the websocket of each connected player
+# until both players are connected and we can start the game.
+WEBSOCKETS: dict[Player, WebSocketServerProtocol] = {}
+
+# player -> last key pressed down this turn, if any
+LAST_KEYDOWNS: dict[Player, str | None] = {player: None for player in Player}
+
+FIGHTERS: dict[Player, Fighter] = {}  # TODO
+
+# a persistent reference to each keydown listener so they don't get
+# garbage collected
+KEYDOWN_LISTENERS: dict[Player, asyncio.Task] = {}
+
+
+async def _listen_for_keydown(player: Player) -> None:
+    """
+    Listen for keydown and write it to `LAST_KEYDOWNS`.
+
+    We'll use the most recent one per player when it's time for the next position.
+    """
+    websocket = WEBSOCKETS[player]
+
+    while True:
+        message = await websocket.recv()
+        event = json.loads(message)
+
+        if "keydown" in event:
+            LAST_KEYDOWNS[player] = event["keydown"]
+
+
+def _apply_keypress(player: Player) -> None:
+    """
+    Read the last keydown & reset it to None
+    Adjust the motor rates based on the keydown
+    """
+
+    fighter = FIGHTERS[player]
+    keydown = LAST_KEYDOWNS[player]
+    LAST_KEYDOWNS[player] = None  # reset last keydown
+    lmotor = fighter.lleg_motor
+    rmotor = fighter.rleg_motor
+
+    # decay prexisting motor rates toward 0
+    for motor in [lmotor, rmotor]:
+        if motor.rate > 0:
+            motor.rate -= 1
+        if motor.rate < 0:
+            motor.rate += 1
+
+    # update based on keydown
+    if keydown and keydown.lower() == "q":
+        # open legs
+        lmotor.rate = -10
+        rmotor.rate = 10
+    elif keydown and keydown.lower() == "w":
+        # close legs
+        lmotor.rate = 10
+        rmotor.rate = -10
+
+
+def _add_walls(space: pymunk.Space) -> None:
+    walls: list[pymunk.Shape] = [
+        pymunk.Segment(space.static_body, (-10, -10), (-10, HEIGHT + 10), 10),
+        pymunk.Segment(space.static_body, (-10, -10), (WIDTH + 10, -10), 10),
+        pymunk.Segment(
+            space.static_body, (WIDTH + 10, -10), (WIDTH + 10, HEIGHT + 10), 10
+        ),
+        pymunk.Segment(
+            space.static_body, (-10, HEIGHT + 10), (WIDTH + 10, HEIGHT + 10), 10
+        ),
+    ]
+    for w in walls:
+        w.friction = 0.5
+        w.group = WALL_GROUP
+        w.elasticity = 0.5
+
+    space.add(*walls)
+
+
+async def _broadcast_positions() -> None:
+    """
+    Send the position of each fighter to each player.
+    """
+    event = {
+        player.value: fighter.position_json for player, fighter in FIGHTERS.items()
+    }
+    async with asyncio.TaskGroup() as tg:
+        for websocket in WEBSOCKETS.values():
+            message = json.dumps(event)
+            coroutine = websocket.send(message)
+            tg.create_task(coroutine)
+
+
+def _keydown_exception_handler(task: asyncio.Task) -> None:
+    try:
+        exception = task.exception()
+        if exception:
+            raise exception
+    except asyncio.CancelledError:
+        pass
+
+
+async def play_game(websockets: dict[Player, WebSocketServerProtocol]) -> None:
+    # listen for keypresses in background tasks
+    for player in Player:
+        task = asyncio.create_task(_listen_for_keydown(player))
+        task.add_done_callback(_keydown_exception_handler)
+        KEYDOWN_LISTENERS[player] = task
+
+    space = pymunk.Space()
+    space.gravity = 0, GRAVITY
+    space.add_default_collision_handler()
+    _add_walls(space)
+
+    FIGHTERS[Player.RED] = add_fighter(space, RED_GROUP)
+    FIGHTERS[Player.BLUE] = add_fighter(space, BLUE_GROUP)
+
+    last_frame = time()
+    while True:
+        # sleep until it's time to for the next frame
+        next_frame = last_frame + 1 / FPS
+        await asyncio.sleep(next_frame - time())
+        last_frame = next_frame
+
+        # update each player's motors based on their keypressed
+        for player in Player:
+            _apply_keypress(player)
+
+        # advance physics
+        for _ in range(STEPS_PER_FRAME):
+            space.step(1.0 / (FPS * STEPS_PER_FRAME))
+
+        await _broadcast_positions()
